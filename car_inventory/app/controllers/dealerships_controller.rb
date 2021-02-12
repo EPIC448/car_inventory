4cebe7b1f72183b6aea7a91c51a6ef51de9fa58a
epic0448 <samuelogundiran@Yahoo.com>
2021-02-12 13:56:06 -0500

    dealerships controller configured

--- car_inventory/app/controllers/dealerships_controller.rb
+++ car_inventory/app/controllers/dealerships_controller.rb
@@ -1,18 +1,20 @@
 class DealershipsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_dealership, only: %i[ show edit update destroy ]
 
   # GET /dealerships or /dealerships.json
   def index
-    @dealerships = Dealership.all
+    @dealerships = current_user.dealerships
   end
 
   # GET /dealerships/1 or /dealerships/1.json
   def show
+  #  @inventory = @dealerships.inventorys.build
   end
 
   # GET /dealerships/new
   def new
-    @dealership = Dealership.new
+    @dealership = current_user.dealerships.build
   end
 
   # GET /dealerships/1/edit
@@ -21,7 +23,7 @@ class DealershipsController < ApplicationController
 
   # POST /dealerships or /dealerships.json
   def create
-    @dealership = Dealership.new(dealership_params)
+    @dealership = current_user.dealerships.build(dealership_params)
 
     respond_to do |format|
       if @dealership.save
@@ -59,11 +61,11 @@ class DealershipsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_dealership
-      @dealership = Dealership.find(params[:id])
+      @dealership = current_user.dealerships.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def dealership_params
-      params.require(:dealership).permit(:name, :address, :user_id)
+      params.require(:dealership).permit(:name, :address)
     end
 end
