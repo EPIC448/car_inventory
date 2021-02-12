c922540241fa27de00c4b3fff7855328093db89c
epic0448 <samuelogundiran@Yahoo.com>
2021-02-12 13:09:20 -0500

    created the dealership class

---
+++ car_inventory/db/migrate/20210212180829_create_dealerships.rb
@@ -0,0 +1,11 @@
+class CreateDealerships < ActiveRecord::Migration[6.1]
+  def change
+    create_table :dealerships do |t|
+      t.string :name
+      t.text :address
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
