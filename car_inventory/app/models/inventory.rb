8c3bae571782d28ba5b6ec87c793bde52846bb97
epic0448 <samuelogundiran@Yahoo.com>
2021-02-12 15:26:53 -0500

    inventories controller updated

--- car_inventory/app/models/inventory.rb
+++ car_inventory/app/models/inventory.rb
@@ -1,3 +1,11 @@
 class Inventory < ApplicationRecord
   belongs_to :dealership
+
+  validates :status, inclusion: {in:['old', 'new']}
+
+  STATUS_OPTIONS = [
+    ['Car Status', 'car-status'],
+    ['New', 'new'],
+    ['Old', 'old'],
+  ]
 end
