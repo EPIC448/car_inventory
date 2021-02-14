bb7f1de3f06bf4339436614187a8e5460eae1a4a
epic0448 <samuelogundiran@Yahoo.com>
2021-02-14 10:38:22 -0500

    Admin access to create inventory

--- car_inventory/app/models/dealership.rb
+++ car_inventory/app/models/dealership.rb
@@ -4,13 +4,5 @@ class Dealership < ApplicationRecord
   has_many :inventories
   belongs_to :user
 
-  def end_of_month
-
-    if last_day = ::Time.days_in_month(month, year)
-      last_hour(days_since(last_day - day))
-      inventories.each do |inventory|
-      return inventory.price - inventory.price * 0.02
-      end
-    end
- end
+  
 end 
