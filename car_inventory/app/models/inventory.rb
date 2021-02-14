48a5072d1e1e97fae74e06daf20d4b0c99eae26b
epic0448 <samuelogundiran@Yahoo.com>
2021-02-14 10:47:22 -0500

    end of the month(prices refactored)

--- car_inventory/app/models/inventory.rb
+++ car_inventory/app/models/inventory.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+require 'active_support/core_ext' 
+
 
 class Inventory < ApplicationRecord
   belongs_to :dealership
@@ -10,4 +12,16 @@ class Inventory < ApplicationRecord
     %w[New new],
     %w[Old old]
   ]
+
+  def end_of_month_price
+      time = Time.new
+      inventory = Inventory.new
+      # days of the month end on the 31 or 30 or 29
+      if time.day  != 31 || time.day  != 30 ||time.day  != 29
+         inventory.price   
+     else
+        inventory.price = inventory.price * 0.02
+    end 
+   end
+    
 end
