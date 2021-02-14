733fc8b3aaf0335e66a39d1173b2d08b31e934f1
epic0448 <samuelogundiran@Yahoo.com>
2021-02-13 21:40:18 -0500

    wrote, test and pass test for models inventory and dealership

--- car_inventory/spec/models/dealership_spec.rb
+++ car_inventory/spec/models/dealership_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Dealership, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+#  before(:each) do
+#   @dealership = Dealership.create(name: "Dealership name", address: "123 street address", user: user.id)
+#  end
+
+#   describe "creation of dealership" do
+#     it "should have one dealership created after required inforamtion is enter"
+#     expect(Dealership.all.count).to eq(1)
+       
+#   end
+
+
+
+  it "is valid with out NAME which is a string" do
+    dealership = Dealership.new(name: nil)
+    expect(dealership).to_not be_valid
+  end
+
+  it "is not valid without a ADDRESS which is a string" do
+    dealership = Dealership.new(address: nil)
+    expect(dealership).to_not be_valid
+  end
+  
+
 end
+
+
--- car_inventory/spec/models/inventory_spec.rb
+++ car_inventory/spec/models/inventory_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Inventory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with out CARMODEL which is a string" do
+    inventory = Inventory.new(carmodel: nil)
+    expect(inventory).to_not be_valid
+  end
+  it "is valid with out PRICE which is a integer" do
+    inventory = Inventory.new(price: nil)
+    expect(inventory).to_not be_valid
+  end
+  it "is valid with out STATUS which is a string" do
+    inventory = Inventory.new(status: nil)
+    expect(inventory).to_not be_valid
+  end
+
 end
