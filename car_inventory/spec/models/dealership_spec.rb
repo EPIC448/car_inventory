5899313b9b56bfca7303ac03f4ba7fc0dc8415e5
epic0448 <samuelogundiran@Yahoo.com>
2021-02-13 22:31:06 -0500

    rails helper and dealership spec passing

--- car_inventory/spec/models/dealership_spec.rb
+++ car_inventory/spec/models/dealership_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Dealership, type: :model do
 
+
   it "is valid with out NAME which is a string" do
     dealership = Dealership.new(name: nil)
     expect(dealership).to_not be_valid
@@ -12,9 +13,7 @@ RSpec.describe Dealership, type: :model do
     expect(dealership).to_not be_valid
   end
   
-  describe "Associations" do
-    it { should belong_to(:user).without_validating_presence }
-  end
+
 end
 
 
