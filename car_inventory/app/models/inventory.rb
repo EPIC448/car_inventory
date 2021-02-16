513748f222f1adfee328607a5fd2a9b31410ab86
epic0448 <samuelogundiran@Yahoo.com>
2021-02-16 08:45:32 -0500

    inventory model need refactor

--- car_inventory/app/models/inventory.rb
+++ car_inventory/app/models/inventory.rb
@@ -15,23 +15,24 @@ class Inventory < ApplicationRecord
 
   # there are 12 months in a year  // Currently stack overfollow
    
-   Inventory.all.each do |inventory|
+#    Inventory.all.each do |inventory|
 
-  def price
-    #  Price reduce by 2% every month
-   time = Time.new
-   time_of_month = time.month
+#   def price
+#     #  Price reduce by 2% every month
+#    time = Time.new
+#    time_of_month = time.month
    
-   for i in 1..time.month
-    break i if i < time.month
-    price = price - (price * 0.02)
-      self.price    
-     end
-     return price
-   end
+#    for i in 1..time.month
+#     break  if i == time.month
+#    return  self.price = self.price - (self.price * 0.02)
+#    else
+#     return    price
+#    end
+#   end
 
-  end
-  
-  
+#   end
+#  end
 
+  
 end
+#  break i if i < time.month
