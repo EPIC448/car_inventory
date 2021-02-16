a4341516931e7157daf3edabf3afbe736f630bb8
epic0448 <samuelogundiran@Yahoo.com>
2021-02-16 16:47:14 -0500

    devise sign-out button working

--- car_inventory/config/routes.rb
+++ car_inventory/config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
   end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   root to: 'dealerships#index'
   # root to: "home#index"  // suggested by devise
 end
