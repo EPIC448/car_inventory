d6de91413ffa2a8e2f0fe93adb3ab7599e202fa8
epic0448 <samuelogundiran@Yahoo.com>
2021-02-16 16:38:40 -0500

    stylesheet was added for buttons for all views

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -5,7 +5,6 @@ require 'active_support/core_ext'
 # Inventory of all the cars that has being created
 class Inventory < ApplicationRecord
   belongs_to :dealership
-
   validates :status, inclusion: { in: %w[old new] }
 
   STATUS_OPTIONS = [
@@ -13,4 +12,27 @@ class Inventory < ApplicationRecord
     %w[New new],
     %w[Old old]
   ].freeze
+
+  # there are 12 months in a year  // Currently stack overfollow
+   
+#    Inventory.all.each do |inventory|
+
+#   def price
+#     #  Price reduce by 2% every month
+#    time = Time.new
+#    time_of_month = time.month
+   
+#    for i in 1..time.month
+#     break  if i == time.month
+#    return  self.price = self.price - (self.price * 0.02)
+#    else
+#     return    price
+#    end
+#   end
+
+#   end
+#  end
+
+  
 end
+#  break i if i < time.month
