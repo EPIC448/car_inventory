981cac9a5a4340b4df98746db1be1974eba7de03
epic0448 <samuelogundiran@Yahoo.com>
2021-02-17 10:47:54 -0500

    update ruby version

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -12,35 +12,24 @@ RSpec.describe Inventory, type: :model do
     expect(inventory).to_not be_valid
   end
 
-  context 'reduce price by %20 at the end of each month' do
-    time = Time.new
-    inventory = Inventory.new(price: nil)
+  it 'check that price is not empty' do
+    inventory = Inventory.new(status: nil)
+    expect(inventory).to_not be == 'nil'
+  end
 
-    (1..time.month).each do |i|
-      it "inventory price should be #{time.month} at month #{i} " do
-        result = inventory.price - (inventory.price * 0.02)
-        expect(result).to eq(inventory.price(-(inventory.price * 0.02)))
+  context 'reduce price by %20 at the end of each month' do
+    it 'Expect to monthly_price method in inventory model to return a price' do
+      inventory = Inventory.new
+      expect(inventory.monthly_price).to_not be == 'nil'
+    end
+
+    it 'Expect to monthly_price method  to return new price at the change of the month' do
+      time = Time.new
+
+      if time.month <= 12
+        inventory = Inventory.new
+        expect(inventory.monthly_price).eql?(inventory.price)
       end
     end
   end
-
-  it 'is valid with out STATUS which is a string' do
-    inventory = Inventory.new(status: nil)
-    expect(inventory).to_not be_valid
-  end
 end
-# def price(price,n)
-#   time = Time.new
-#   puts time.month
-#   #iterate over number of months & perform calulation
-#   #for i in 1..n  work like a while loop
-
-#   for i in 1..time.month
-#   price = price - (price * 0.02)
-#   puts "Month #{i} price $#{price.round(2)}"
-# end
-# return price
-
-# end
-
-#   price(100,12)
