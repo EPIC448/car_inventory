86bcb6e8f1d1345e4395206e589230d1dde18bec
epic0448 <samuelogundiran@Yahoo.com>
2021-02-16 22:49:10 -0500

    refactor withrobocop

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -13,26 +13,19 @@ class Inventory < ApplicationRecord
     %w[Old old]
   ].freeze
 
-  # there are 12 months in a year  // Currently stack overfollow
-   
-#    Inventory.all.each do |inventory|
+  # Assuming we start on month 1 january,
+  #  Price reduce by 2% every month
 
-#   def price
-#     #  Price reduce by 2% every month
-#    time = Time.new
-#    time_of_month = time.month
-   
-#    for i in 1..time.month
-#     break  if i == time.month
-#    return  self.price = self.price - (self.price * 0.02)
-#    else
-#     return    price
-#    end
-#   end
-
-#   end
-#  end
-
-  
+  def monthlyPrice
+    #  Price reduce by 2% every month
+    time = Time.now
+    if time.month <= 12 && !price.nil?
+      (1..time.month).each do |_i|
+        self.price = price - (price * 0.02)
+        return price
+      end
+    else
+      price # inventory.price
+    end
+  end
 end
-#  break i if i < time.month
--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Inventory, type: :model do
-  
   it 'is valid with out CARMODEL which is a string' do
     inventory = Inventory.new(carmodel: nil)
     expect(inventory).to_not be_valid
@@ -12,32 +11,30 @@ RSpec.describe Inventory, type: :model do
     inventory = Inventory.new(price: nil)
     expect(inventory).to_not be_valid
   end
- 
+
   context 'reduce price by %20 at the end of each month' do
-    time = Time.new 
+    time = Time.new
     inventory = Inventory.new(price: nil)
-   
-    for i in 1..time.month
-    it "inventory price should be #{time.month} at month #{i} " do
-    result = inventory.price - (inventory.price * 0.02)
-      expect(result).to eq (inventory.price -(inventory.price * 0.02) )
+
+    (1..time.month).each do |i|
+      it "inventory price should be #{time.month} at month #{i} " do
+        result = inventory.price - (inventory.price * 0.02)
+        expect(result).to eq(inventory.price(-(inventory.price * 0.02)))
+      end
     end
   end
-  end 
 
-
-it 'is valid with out STATUS which is a string' do
+  it 'is valid with out STATUS which is a string' do
     inventory = Inventory.new(status: nil)
     expect(inventory).to_not be_valid
   end
-
 end
 # def price(price,n)
 #   time = Time.new
-#   puts time.month 
-#   #iterate over number of months & perform calulation 
+#   puts time.month
+#   #iterate over number of months & perform calulation
 #   #for i in 1..n  work like a while loop
-  
+
 #   for i in 1..time.month
 #   price = price - (price * 0.02)
 #   puts "Month #{i} price $#{price.round(2)}"
@@ -45,6 +42,5 @@ end
 # return price
 
 # end
-  
-  
+
 #   price(100,12)
